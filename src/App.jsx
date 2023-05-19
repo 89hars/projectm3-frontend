4bbf8f72021e82b4f35d61da9fff95267b788459
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-05-19 22:38:41 +0800

    login & signup page created, app.jsx updated w/login&signup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import DetailsPage from './pages/DetailsPage'
-import UpdatePage from './pages/UpdatePage'
-import CreatePage from './pages/CreatePage'
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import UpdatePage from "./pages/UpdatePage";
+import CreatePage from "./pages/CreatePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
- function App() {
- /*    const APITestConnection = async () => {
+function App() {
+  /*    const APITestConnection = async () => {
     const response = await fetch('http://localhost:5005/api')
     const parsed = await response.json()
     console.log(parsed)
@@ -14,19 +16,19 @@ import CreatePage from './pages/CreatePage'
 */
   useEffect(() => {
     // APITestConnection()
-  }, [])
+  }, []);
 
-
-   // Routes To CRUD, delete  will be handle inside Details
+  // Routes To CRUD, delete  will be handle inside Details
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/details' element={<DetailsPage />} />
-      <Route path='/update' element={<UpdatePage />} />
-      <Route path='/create' element={<CreatePage />} />
-
+      <Route path="/" element={<HomePage />} />
+      <Route path="/details" element={<DetailsPage />} />
+      <Route path="/update" element={<UpdatePage />} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
